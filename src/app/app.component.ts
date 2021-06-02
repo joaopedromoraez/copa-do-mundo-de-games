3e912f7aeeaa4b0fd17de86f711cdee0043a8f29
João Pedro Moraes <joaopedromoraes@outlook.com>
2021-06-02 20:27:47 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Copa-Do-Mundo-De-Games-Lambda3';
+}
