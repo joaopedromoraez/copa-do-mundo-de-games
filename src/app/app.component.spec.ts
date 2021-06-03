5c7d7505d18cf8c23ad3131b4a396b95cba77a8a
João Pedro Moraes <joaopedromoraes@outlook.com>
2021-06-02 21:30:50 -0300

    imprvr: sanatiza html do componente raiz e atualiza arquvivo de teste

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,22 +14,10 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('Deve Criar o AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Copa-Do-Mundo-De-Games-Lambda3'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Copa-Do-Mundo-De-Games-Lambda3');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Copa-Do-Mundo-De-Games-Lambda3 app is running!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Copa-Do-Mundo-De-Games-Lambda3';
-}
+export class AppComponent {}
