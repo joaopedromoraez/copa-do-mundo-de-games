bb9cbbd8736c67911d16b132513d4e6787f6fa97
João Pedro Moraes <joaopedromoraes@outlook.com>
2021-06-05 23:10:01 -0300

    feat: cria view de seleção de games

--- src/app/modules/fase-selecao/fase-selecao.component.ts
+++ src/app/modules/fase-selecao/fase-selecao.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Game } from './models/game';
+import { Lambda3Service } from './services/lambda3.service';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-fase-selecao',
@@ -7,9 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaseSelecaoComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['select', 'id', 'titulo', 'nota', 'ano'];
+
+  games = new MatTableDataSource<Game>();
+
+  selection = new SelectionModel<Game>(true, []);
+
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  constructor(
+    readonly service: Lambda3Service,
+  ) { }
 
   ngOnInit(): void {
+    this.games.sort = this.sort;
+    this.service.getGames().subscribe(res=>this.games.data = res);
+  }
+
+  consoleName(id: string) {
+    return id.split('/')[1]
+            .split('-')
+            .map(name=>`${name[0].toUpperCase()}${name.substring(1)}`)
+            .join(' ')
   }
 
 }
--- src/app/modules/fase-selecao/fase-selecao.module.ts
+++ src/app/modules/fase-selecao/fase-selecao.module.ts
@@ -3,8 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { FaseSelecaoRoutingModule } from './fase-selecao-routing.module';
 import { FaseSelecaoComponent } from './fase-selecao.component';
-import { Lambda3Service } from './service/lambda3.service';
+import { Lambda3Service } from './services/lambda3.service';
 import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -13,7 +19,13 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     CommonModule,
     FaseSelecaoRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatTableModule,
+    MatSortModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatDividerModule,
   ],
   providers: [
     Lambda3Service,
