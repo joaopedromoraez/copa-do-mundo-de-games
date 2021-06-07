ef543baef9c71baa00dfd8cd6cf96e779fc89c65
João Pedro Moraes <joaopedromoraes@outlook.com>
2021-06-07 19:34:55 -0300

    feat: cria logica de classificação dos times

--- src/app/modules/resultado-final/resultado-final.component.ts
+++ src/app/modules/resultado-final/resultado-final.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Game } from '@shared/models/game';
+import { DataService } from '@shared/services/data.service';
 
 @Component({
   selector: 'app-resultado-final',
@@ -7,9 +10,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultadoFinalComponent implements OnInit {
 
-  constructor() { }
+  gamesSelected: Game[];
+  matchups;
+  secondLevel;
+  thirdLevel;
+  winnerLevel;
+
+  constructor(
+    private storage: DataService,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
+
+    this.gamesSelected = this.storage.get('GAMES_SELECTED');
+
+    if (this.gamesSelected === null) this.router.navigateByUrl('/fase-selecao');
+
+    this.gamesSelected.sort((a, b) => a.titulo.localeCompare(b.titulo));
+
+    this.matchups = this.matchUp(this.gamesSelected);
+
+    this.secondLevel = this.matchups.map(([a, b]) => {
+      const [winner] = this.winnerBetween(a, b);
+      return {
+        winner,
+        children: [
+          { winner: a, children: [] },
+          { winner: b, children: [] },
+        ],
+      };
+    });
+
+    this.thirdLevel = this.matchEachTwo(this.secondLevel).map(([a, b]) => {
+      const [winner] = this.winnerBetween(a.winner, b.winner);
+      return {
+        winner,
+        children: [a, b],
+      };
+    });
+
+    this.winnerLevel = this.matchEachTwo(this.thirdLevel).map(([a, b]) => {
+      const [winner] = this.winnerBetween(a.winner, b.winner);
+      return {
+        winner,
+        children: [a, b],
+      };
+    });
+  }
+
+  matchProp(a, b, prop) {
+    if (a[prop] > b[prop]) {
+      return [a, b];
+    }
+    if (a[prop] < b[prop]) {
+      return [b, a];
+    }
+    return false;
+  }
+
+  matchUp(games) {
+    return Array.from({ length: Math.floor(games.length / 2) }, (_, i) => [
+      games[i],
+      games[games.length - 1],
+    ]);
+  }
+
+  matchEachTwo(games) {
+    return Array.from({ length: Math.floor(games.length / 2) }, (_, i) => [
+      games[i * 2],
+      games[i * 2 + 1],
+    ]);
+  }
+
+  matchNameOrder(a, b, prop) {
+    if (a[prop].localeCompare(b[prop]) === -1) {
+      return [a, b];
+    }
+    return [b, a];
+  }
+
+  winnerBetween(a, b) {
+    return (
+      this.matchProp(a, b, "nota") ||
+      this.matchProp(a, b, "ano") ||
+      this.matchNameOrder(a, b, "titulo")
+    );
   }
 
 }
